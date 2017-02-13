c200019462d68908bf612c1d92a783ea57d988e6
Roger <rogerul_2000@hotmail.com>
2017-02-13 07:54:08 -0500

    Validacion de formulario 90%

--- js/script.js
+++ js/script.js
@@ -23,57 +23,84 @@ $(document).ready(function(){
 	/************** VALIDACION DE FORUMLARIO*******************/
 
 
-var expr_mail = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;  //expresion regular par email
-
-	$('#nombre').focusout(function () {
+	//validar nombre
+	$('#nombre').focusout(function (event) {
+		event.stopImmediatePropagation();	
 		var nombre=$('#nombre').val();
 			if(nombre=="" || !isNaN(nombre)){
-				$(this).stop().after("<span class='alerta'>Escribir nombre (no acepta caracteres números)</span>");
-				$(this).next().stop().delay(1000).fadeOut(300);
+				$(this).after("<span class='alerta' style=font-size:12px;>Campo obligatorio (no acepta caracteres númericos)</span>").stop();
 				$(this).val("");
-					document.getElementById("nombre").focus()
+			} else{
+				$(this).next().stop().fadeOut(300);
 			}
 	});
 
+	$('#nombre').focusin(function(){
+		$(this).next().stop().fadeOut(300);
+	});	
+
+
+	//validar email
+	var expr_mail = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;  //expresion regular par email
 	$('#email').focusout(function() {
 		var correo=$('#email').val();
-		if(correo == "" || !expr_mail.test(correo)){  // verifcando si correo tiene los campor requeridos
-			$(this).stop().after("<span class='alerta'>Debe contener el simbolo @ </span>");
-			$(this).next().stop().delay(1000).fadeOut(300);
+		if(correo == "" || !expr_mail.test(correo)){  // verifcando si correo tiene los campos requeridos
+			$(this).stop().after("<span class='alerta' style=font-size:12px;>Campo obligatorio debe contener el simbolo @ </span>");
 			$(this).val("");
-			document.getElementById("email").focus();
+			return false;
+		} else{
+			$(this).next().stop().fadeOut(300);
 		}
 	});
 
+	$('#email').focusin(function(){
+		$(this).next().stop().fadeOut(300);
+	});
 
+
+	//validar clave
 		$('#clave').focusout(function (){
 			var clave=$('#clave').val();
 			if(clave.length<4){
-				$('#clave').stop().after("<span class='alerta'>minimo 4 caracteres</span>");
-				$('#clave').next().stop().delay(1000).fadeOut(400);
-				 document.getElementById("clave").focus();
+				$(this).stop().after("<span class='alerta' style=font-size:12px;>La contraseña debe contener entre mínimo 4 caracteres</span>");
+				$(this).val("");
+				return false;
+			} else{
+				$(this).next().stop().fadeOut(300);
 			}
 		});
 
-		//PALABRAS SOCECES
+		$('#clave').focusin(function(){
+			$(this).next().stop().fadeOut(300);
+		});
 
-		var groserias=["idiota", "imbecil", "carajo", "mierda", "estupido"];
-		var signos="#@?!%#&";  
+
+		//VALIDAR TEXTAREA COMENTARIO - PALABRAS SOECES
+
+		var groserias=["idiota", "imbecil", "carajo", "mierda", "estupido", "estupida", "puta"];
+		var signos="######";  
 		
-		$('#comentario').change(function(){
+		$('#comentario').keyup(function() {
 			var comentario=$('#comentario').val();
 			var palabras_array=comentario.split(" ");
 
-			for(var i=0; i<groserias.length; i++){
-				for (var p=0; p<palabras_array.length; p++) {
-					if(palabras_array[p]==groserias[i]){
-						var palabra_g=palabras_array[p];
-							palabra_g='######';
+				for(var i=0; i<groserias.length; i++){
+					for (var p=0; p<palabras_array.length; p++) {
+						if(palabras_array[p].toLowerCase()==groserias[i]){
+							palabras_array[p]=signos;
+						} 					
 					}
 				}
-			}
-			alert(palabra_g);
+			$('#comentario').val(palabras_array);
 		});
+
+
+		// $('#comentario').keyup(function(){
+		// 	corregirPalabra();
+		// }).keyup();
+
+
+			
 
 });
 
